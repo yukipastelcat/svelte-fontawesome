cb2ae5d95344a8479ed1852ca1b94739e345a838
Kirill Sedunov <kirill.sedunov@outlook.com>
2020-01-30 17:53:26 +0300

    Changed bundler to rollup
    Changed bundler to rollup since it is easier

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+﻿import pluginSvelte from 'rollup-plugin-svelte';
+import pluginResolve from '@rollup/plugin-node-resolve';
+import pluginCommonJs from '@rollup/plugin-commonjs';
+import { terser as pluginTerser } from "rollup-plugin-terser";
+import pluginBabel from 'rollup-plugin-babel';
+
+import pkg from './package.json';
+
+const name = 'FontawesomeSvelte';
+const globals = {
+  '@fortawesome/fontawesome-svg-core': 'FontAwesome'
+}
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'umd',
+      name,
+      globals,
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      globals,
+    }
+  ],
+  plugins: [
+    pluginSvelte({
+      include: 'src/**/*.svelte',
+    }),
+    pluginResolve(),
+    pluginCommonJs(),
+    pluginBabel(),
+    pluginTerser({
+      compress: {
+        drop_console: false
+      }
+    }),
+  ]
+}
